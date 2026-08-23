7df95f3e618daca57567b8329dfab4cba587bd86
Giovanni Giannone <Johnjohngiannone@gmail.com>
2026-08-23 16:40:12 -0400

    Created player.py, updated main.py to draw player, and added new constants

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import pygame
-import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
-    pygame.init()                                                                       # initializing pygame
+    pygame.init()                                                             # initializing pygame
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print (f"Screen width: {constants.SCREEN_WIDTH}")
-    print (f"Screen height: {constants.SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)) # creates new instance of the GUI window
-    clock = pygame.time.Clock()                                                         # Creating new Clock object
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))           # creates new instance of the GUI window
+    clock = pygame.time.Clock()                                               # Creating new Clock object
     dt = 0.0
-
-    while True:                                                                         # needs infinite loop for gameloop
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)                          # Adds player to middle of screen
+    while True:                                                               # needs infinite loop for gameloop
         log_state()
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:                                               # Will kill program if user exits out of window
-                return
-
-        screen.fill("black")                                                            # fills the window with black screen
-        pygame.display.flip()                                                           # Refresh the screen. Has to be always at the end of the loop
-        dt = clock.tick(60) / 1000                                                      # calculating delta time for fps
+            if event.type == pygame.QUIT:                                     # Will kill program if user exits out of window
+                return  
+        screen.fill("black")                                                  # fills the window with black screen
+        player.draw(screen)                                                   # draw the player for every loop
+        pygame.display.flip()                                                 # Refresh the screen. Has to be always at the end of the loop
+        dt = clock.tick(60) / 1000                                            # calculating delta time for fps
         
 
 
