669b03dd27e69830395409bcb535824281e90024
Giovanni Giannone <Johnjohngiannone@gmail.com>
2026-08-23 18:15:16 -0400

    Added physics to move up and down

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape): # Player class
@@ -16,7 +16,7 @@ class Player(CircleShape): # Player class
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    def draw(self, screen: object):                     # Draws player (as a triangle)
+    def draw(self, screen: object) -> None:             # Draws player (as a triangle)
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
     def rotate(self, dt: float) -> None:                # updates rotation of character based on delta time
@@ -29,3 +29,13 @@ class Player(CircleShape): # Player class
             self.rotate((-1 * dt))
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move((-1 * dt))
+
+    def move(self, dt) -> None:                         #Added the physics to move up and down; created a vector, rotate vector using players rotation,
+        unit_vector = pygame.Vector2(0,1)               #calc vector on how much player would move in the frame, update position with calculated vector
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
